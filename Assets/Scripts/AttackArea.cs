54ab44c7d7db2d8b21a15cdc591c081ea562d79d
unknown <PS25CBailey@efcts.us>
2023-12-11 10:45:25 -0500

    .

--- Assets/Scripts/AttackArea.cs
+++ Assets/Scripts/AttackArea.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AttackArea : MonoBehaviour
 {
-    private int damage = 100;
+    private int damage = 100000;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
